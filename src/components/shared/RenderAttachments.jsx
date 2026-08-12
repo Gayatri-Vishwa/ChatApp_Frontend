0ecd19cbfde53aa5638363376d14f5261cada4e9
Gayatri Vishwa <blossomjasmine02@gmail.com>
2026-08-12 18:17:51 +0530

    ready

--- src/components/shared/RenderAttachments.jsx
+++ src/components/shared/RenderAttachments.jsx
@@ -12,7 +12,7 @@ switch (file) {
 
   case "audio":
   case "mp3":
-    return <audio src={url} preload="none" controls   style={{ width: "400px", maxWidth: "100%" }}/>;
+    return <audio src={url} preload="none" controls   style={{ width: "min(260px, 100%)" }}/>;
 
   case "image":
   case "jpg":
